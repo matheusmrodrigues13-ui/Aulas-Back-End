a8b5bc956d10d24ddc3236e0d544fabdf29708dd
matheusmrodrigues13-ui <matheus.m.rodrigues13@aluno.senai.br>
2026-07-29 15:57:22 -0300

    Aulas-Back-End

---
+++ carteira.php
@@ -0,0 +1,10 @@
+<?php
+$idade = 20;
+$temcarteira = true;
+
+if($idade >= 18 && $temcarteira == true){
+    echo("Você pode dirigir!");
+} else {
+    echo("Você não pode dirigir.");
+}
+?>
---
+++ subtracao.php
@@ -0,0 +1,7 @@
+<?php
+echo"Realizando os cáuculos das variáveis: <br>";
+
+$variavel1 = 20;
+$variavel_2 = 300;
+echo("$variavel_2 - $variavel1 = " . ($variavel_2 - $variavel1 . "<br>"));
+?>
