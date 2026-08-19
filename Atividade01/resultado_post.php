17d6f306eee98d9541e75a46a60bb02fcd3c2199
matheusmrodrigues13-ui <matheus.m.rodrigues13@aluno.senai.br>
2026-08-19 11:33:25 -0300

    19-08-2026

--- Atividade01/resultado_post.php
+++ Atividade01/resultado_post.php
@@ -1,6 +1,6 @@
 <?php 
-$resultado1 = $_POST[('n1')];
-$resultado2 = $_POST[('n2')];
+$resultado1 = $_POST['n1'];
+$resultado2 = $_POST['n2'];
 $resultado = $resultado1 + $resultado2;
 echo "O resultado da soma é: $resultado";
 
